2400753bef727cf40cd648a97f150ee5edf2fe77
hidekiiwasa <Spider321011@hotmail.com>
2025-06-12 20:04:35 -0300

    feat: user atributes

--- src/main/java/com/bethunter/bethunter_api/model/User.java
+++ src/main/java/com/bethunter/bethunter_api/model/User.java
@@ -1,4 +1,21 @@
 package com.bethunter.bethunter_api.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String login;
+    private String password;
+    private String name;
+    private String cellphone;
+    private String email;
+    private int points;
+    private int rank;
+
+
 }
