edda21adaa8d6c6b41bfbc3a672b9900a4641ac0
hidekiiwasa <Spider321011@hotmail.com>
2025-06-13 20:02:36 -0300

    feat: user authentication methods and constructor

--- src/main/java/com/bethunter/bethunter_api/model/User.java
+++ src/main/java/com/bethunter/bethunter_api/model/User.java
@@ -1,10 +1,19 @@
 package com.bethunter.bethunter_api.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Entity
-@Table(name = "user")
-public class User {
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@Entity(name = "users")
+@NoArgsConstructor
+@Getter
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -12,10 +21,53 @@ public class User {
     private String login;
     private String password;
     private String name;
-    private String cellphone;
     private String email;
+    private String cellphone;
     private int points;
-    private int rank;
+    private int placement;
 
+    public User(String login, String password, String name, String email, String cellphone) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.cellphone = cellphone;
+        this.points = 0;
+        this.placement = 0;
+    }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
