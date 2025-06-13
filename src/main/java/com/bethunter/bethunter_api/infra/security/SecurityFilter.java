7bf355693da2672234aaf71cecca61837e0cea37
hidekiiwasa <Spider321011@hotmail.com>
2025-06-13 19:57:32 -0300

    feat: security filter

---
+++ src/main/java/com/bethunter/bethunter_api/infra/security/SecurityFilter.java
@@ -0,0 +1,45 @@
+package com.bethunter.bethunter_api.infra.security;
+
+import com.bethunter.bethunter_api.repository.RepositoryUser;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private ServiceToken serviceToken;
+    @Autowired
+    private RepositoryUser repositoryUser;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var login = serviceToken.validateToken(token);
+            UserDetails user = repositoryUser.findByLogin(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader != null) return null;
+
+        return authHeader.replace("Bearer ", "");
+    }
+}
