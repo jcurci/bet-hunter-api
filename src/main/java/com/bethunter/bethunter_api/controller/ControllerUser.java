654f7d662d1e003e93786889f305e0aa4724576d
hidekiiwasa <Spider321011@hotmail.com>
2025-06-12 21:31:02 -0300

    on progress...

--- src/main/java/com/bethunter/bethunter_api/controller/ControllerUser.java
+++ src/main/java/com/bethunter/bethunter_api/controller/ControllerUser.java
@@ -1,12 +1,15 @@
 package com.bethunter.bethunter_api.controller;
 
+import com.bethunter.bethunter_api.dto.UserRequestCreate;
 import com.bethunter.bethunter_api.dto.UserResponse;
 import com.bethunter.bethunter_api.service.ServiceUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("users")
@@ -16,5 +19,8 @@ public class ControllerUser {
     private ServiceUser serviceUser;
 
     @PostMapping
-    public ResponseEntity<UserResponse>
+    public ResponseEntity<String> createUser(@RequestBody UserRequestCreate dto) {
+        return ResponseEntity.status(201).body("test");
+    }
+
 }
