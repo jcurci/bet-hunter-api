ddb70e118f5170e56bfff4fb6bef92873c0a086d
hidekiiwasa <Spider321011@hotmail.com>
2025-06-13 19:57:03 -0300

    feat: user repository

--- src/main/java/com/bethunter/bethunter_api/repository/RepositoryUser.java
+++ src/main/java/com/bethunter/bethunter_api/repository/RepositoryUser.java
@@ -1,4 +1,9 @@
 package com.bethunter.bethunter_api.repository;
 
-public interface RepositoryUser {
+import com.bethunter.bethunter_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface RepositoryUser extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
 }
