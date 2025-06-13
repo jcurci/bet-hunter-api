c50844fa97522a3a5c987c394539d8c5abde62a3
hidekiiwasa <Spider321011@hotmail.com>
2025-06-13 19:56:20 -0300

    feat: authentication controller

---
+++ src/main/java/com/bethunter/bethunter_api/controller/ControllerAuthentication.java
@@ -0,0 +1,53 @@
+package com.bethunter.bethunter_api.controller;
+
+import com.bethunter.bethunter_api.dto.AuthenticationRequestLogin;
+import com.bethunter.bethunter_api.dto.AuthenticationRequestRegister;
+import com.bethunter.bethunter_api.dto.LoginResponse;
+import com.bethunter.bethunter_api.infra.security.ServiceToken;
+import com.bethunter.bethunter_api.model.User;
+import com.bethunter.bethunter_api.repository.RepositoryUser;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class ControllerAuthentication {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private RepositoryUser repositoryUser;
+    @Autowired
+    private ServiceToken serviceToken;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationRequestLogin dto) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.login(), dto.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = serviceToken.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.status(200).body(new LoginResponse(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid AuthenticationRequestRegister dto) {
+        if (repositoryUser.findByLogin(dto.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(dto.password());
+        User user = new User(dto.login(), encryptedPassword, dto.name(),
+                dto.email(), dto.cellphone());
+
+        repositoryUser.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+}
