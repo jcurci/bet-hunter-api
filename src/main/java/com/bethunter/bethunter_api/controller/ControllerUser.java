446a1f0dddf7586588d53a328ef2dac6efbdcf86
hidekiiwasa <Spider321011@hotmail.com>
2025-06-14 22:15:55 -0300

    feat: spin roulete route

--- src/main/java/com/bethunter/bethunter_api/controller/ControllerUser.java
+++ src/main/java/com/bethunter/bethunter_api/controller/ControllerUser.java
@@ -1,14 +1,10 @@
 package com.bethunter.bethunter_api.controller;
 
-import com.bethunter.bethunter_api.dto.UserRequestCreate;
 import com.bethunter.bethunter_api.dto.UserResponse;
 import com.bethunter.bethunter_api.service.ServiceUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,9 +14,9 @@ public class ControllerUser {
     @Autowired
     private ServiceUser serviceUser;
 
-    @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody UserRequestCreate dto) {
-        return ResponseEntity.status(201).body("test");
+    @GetMapping
+    public ResponseEntity<?> spinRoulete(@RequestHeader(name = "Authorization") String token) {
+        return serviceUser.spinRoulete(token);
     }
 
 }
