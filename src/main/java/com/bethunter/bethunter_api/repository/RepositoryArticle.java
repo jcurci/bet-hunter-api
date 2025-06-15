499fac87f31da6486f9eeac302bd19bf6e0f8b43
hidekiiwasa <Spider321011@hotmail.com>
2025-06-14 22:15:27 -0300

    fix: service annotation missing

--- src/main/java/com/bethunter/bethunter_api/repository/RepositoryArticle.java
+++ src/main/java/com/bethunter/bethunter_api/repository/RepositoryArticle.java
@@ -2,6 +2,8 @@ package com.bethunter.bethunter_api.repository;
 
 import com.bethunter.bethunter_api.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositoryArticle extends JpaRepository<Article, String> {
 }
