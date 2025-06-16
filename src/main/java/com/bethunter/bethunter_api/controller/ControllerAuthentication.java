f2646981bcee13f3f72ff7c43edb744b9174364f
hidekiiwasa <Spider321011@hotmail.com>
2025-06-16 18:06:13 -0300

    refactor: moved method logic to service

--- src/main/java/com/bethunter/bethunter_api/controller/ControllerAuthentication.java
+++ src/main/java/com/bethunter/bethunter_api/controller/ControllerAuthentication.java
@@ -1,58 +1,42 @@
 package com.bethunter.bethunter_api.controller;
 
-import com.bethunter.bethunter_api.dto.AuthenticationRequestLogin;
-import com.bethunter.bethunter_api.dto.AuthenticationRequestRegister;
-import com.bethunter.bethunter_api.dto.LoginResponse;
+import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestLogin;
+import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestPasswordChange;
+import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestRegister;
+import com.bethunter.bethunter_api.dto.authentication.LoginResponse;
 import com.bethunter.bethunter_api.infra.security.ServiceToken;
 import com.bethunter.bethunter_api.model.User;
 import com.bethunter.bethunter_api.repository.RepositoryUser;
+import com.bethunter.bethunter_api.service.ServiceAuthentication;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
 public class ControllerAuthentication {
 
-    private final RepositoryUser repositoryUser;
-    private final ServiceToken serviceToken;
-    private final AuthenticationManager authenticationManager;
+    @Autowired
+    private ServiceAuthentication serviceAuthentication;
 
-    public ControllerAuthentication(
-            RepositoryUser repositoryUser,
-            ServiceToken serviceToken,
-            AuthenticationManager authenticationManager) {
-        this.repositoryUser = repositoryUser;
-        this.serviceToken = serviceToken;
-        this.authenticationManager = authenticationManager;
-    }
+
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationRequestLogin dto) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
-        var auth = authenticationManager.authenticate(usernamePassword);
-
-        var token = serviceToken.generateToken((User) auth.getPrincipal());
-
-        return ResponseEntity.status(200).body(new LoginResponse(token));
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid AuthenticationRequestLogin dto) {
+        return ResponseEntity.status(200).body(serviceAuthentication.login(dto));
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid AuthenticationRequestRegister dto) {
-        if (repositoryUser.findByEmail(dto.email()).isPresent()) return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> register(@RequestBody @Valid AuthenticationRequestRegister dto) {
+        return serviceAuthentication.register(dto);
+    }
 
-        String encryptedPassword = new BCryptPasswordEncoder().encode(dto.password());
-        User user = new User(dto.email(), encryptedPassword, dto.name(),
-                dto.cellphone());
-
-        repositoryUser.save(user);
-        return ResponseEntity.ok().build();
+    @PutMapping("/password")
+    public ResponseEntity<?> changePassword(@RequestHeader(name = "Authorization") String token, @RequestBody AuthenticationRequestPasswordChange dto) {
+        return serviceAuthentication.changePassword(token, dto);
     }
 }
