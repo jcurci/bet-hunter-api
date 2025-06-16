95f05966600b1213bc64885601dd78786d3faaba
hidekiiwasa <Spider321011@hotmail.com>
2025-06-16 18:02:37 -0300

    feat: removed unused class

--- src/main/java/com/bethunter/bethunter_api/dto/UserRequestCreate.java
+++
@@ -1,7 +0,0 @@
-package com.bethunter.bethunter_api.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserRequestCreate(@NotBlank String email, @NotBlank String password, @NotBlank String name,
-@NotBlank String cellphone) {
-}
