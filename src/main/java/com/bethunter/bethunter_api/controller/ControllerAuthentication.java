1ccfb6cb185bba9459e75dccf53a3cee572f0682
hidekiiwasa <Spider321011@hotmail.com>
2025-09-20 19:15:40 -0300

    refactor: changing how the service return to the controller

--- src/main/java/com/bethunter/bethunter_api/controller/ControllerAuthentication.java
+++ src/main/java/com/bethunter/bethunter_api/controller/ControllerAuthentication.java
@@ -4,6 +4,7 @@ import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestLogin
 import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestPasswordChange;
 import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestRegister;
 import com.bethunter.bethunter_api.dto.authentication.LoginResponse;
+import com.bethunter.bethunter_api.dto.user.UserResponse;
 import com.bethunter.bethunter_api.service.ServiceAuthentication;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +18,21 @@ public class ControllerAuthentication {
     @Autowired
     private ServiceAuthentication serviceAuthentication;
 
-
+    // @CrossOrigin(origins = "*")
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody @Valid AuthenticationRequestRegister dto) {
+        return ResponseEntity.status(201).body(serviceAuthentication.register(dto));
+    }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid AuthenticationRequestLogin dto) {
         return ResponseEntity.status(200).body(serviceAuthentication.login(dto));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid AuthenticationRequestRegister dto) {
-        return serviceAuthentication.register(dto);
-    }
-
     @PutMapping("/password")
-    public ResponseEntity<?> changePassword(@RequestHeader(name = "Authorization") String token, @RequestBody AuthenticationRequestPasswordChange dto) {
-        return serviceAuthentication.changePassword(token, dto);
+    public ResponseEntity<Void> changePassword(@RequestHeader(name = "Authorization") String token, @RequestBody AuthenticationRequestPasswordChange dto) {
+        serviceAuthentication.changePassword(token, dto);
+        return ResponseEntity.ok().build();
     }
-
 
 }
--- src/main/java/com/bethunter/bethunter_api/service/ServiceAuthentication.java
+++ src/main/java/com/bethunter/bethunter_api/service/ServiceAuthentication.java
@@ -4,11 +4,13 @@ import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestLogin
 import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestPasswordChange;
 import com.bethunter.bethunter_api.dto.authentication.AuthenticationRequestRegister;
 import com.bethunter.bethunter_api.dto.authentication.LoginResponse;
-import com.bethunter.bethunter_api.exception.UserNotFound;
+import com.bethunter.bethunter_api.dto.user.UserResponse;
+import com.bethunter.bethunter_api.exception.*;
 import com.bethunter.bethunter_api.infra.security.ServiceToken;
+import com.bethunter.bethunter_api.mapper.UserMapper;
 import com.bethunter.bethunter_api.model.User;
 import com.bethunter.bethunter_api.repository.RepositoryUser;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,19 +18,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ServiceAuthentication {
 
+    // For some reason @Autowired is not working in this class
     private final RepositoryUser repositoryUser;
     private final ServiceToken serviceToken;
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
-
-    public ServiceAuthentication(RepositoryUser repositoryUser, ServiceToken serviceToken, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
-        this.repositoryUser = repositoryUser;
-        this.serviceToken = serviceToken;
-        this.authenticationManager = authenticationManager;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final UserMapper userMapper;
 
     public LoginResponse login(AuthenticationRequestLogin dto) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
@@ -39,35 +37,29 @@ public class ServiceAuthentication {
         return new LoginResponse(token);
     }
 
-    public ResponseEntity<?> register(AuthenticationRequestRegister dto) {
-        if (repositoryUser.findByEmail(dto.email()).isPresent()) return ResponseEntity.badRequest().body("Email already registered");
-        if (repositoryUser.findByCellphone(dto.cellphone()).isPresent()) return ResponseEntity.badRequest().body("Phone already registered");
+    public UserResponse register(AuthenticationRequestRegister dto) {
+        if (repositoryUser.findByEmail(dto.email()).isPresent()) throw new EmailAlreadyRegistered();
+        if (repositoryUser.findByCellphone(dto.cellphone()).isPresent()) throw new PhoneAlreadyRegistered();
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(dto.password());
         User user = new User(dto.email(), encryptedPassword, dto.name(),
                 dto.cellphone());
 
-        repositoryUser.save(user);
-        return ResponseEntity.ok().build();
+        return userMapper.toResponseDTO(repositoryUser.save(user));
     }
 
 
-    public ResponseEntity<?> changePassword(String token, AuthenticationRequestPasswordChange dto) {
+    public void changePassword(String token, AuthenticationRequestPasswordChange dto) {
         var email = serviceToken.validateToken(token.replace("Bearer ", ""));
-        if (email.isEmpty()) {
-            return ResponseEntity.badRequest().body("Invalid or missing token");
-        }
+        if (email.isEmpty()) throw new InvalidToken();
 
         User user = repositoryUser.findByEmail(email).orElseThrow(() -> new UserNotFound());
 
-        if (!passwordEncoder.matches(dto.current_password(), user.getPassword())) {
-            return ResponseEntity.badRequest().body("Wrong password");
-        }
+        if (!passwordEncoder.matches(dto.current_password(), user.getPassword())) throw new PasswordsDoesntMatch();
 
         user.setPassword(passwordEncoder.encode(dto.new_password()));
         repositoryUser.save(user);
 
-        return ResponseEntity.ok().build();
     }
 
 }
