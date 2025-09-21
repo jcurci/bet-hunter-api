ed9a6a6c92424205f2f9537aa26f4904a8f87f84
hidekiiwasa <Spider321011@hotmail.com>
2025-09-21 01:19:16 -0300

    on progress

---
+++ src/main/java/com/bethunter/bethunter_api/model/Account.java
@@ -0,0 +1,21 @@
+//package com.bethunter.bethunter_api.model;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Entity
+//@Table(name = "accounts")
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//public class Account {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.UUID)
+//    private String id;
+//
+//
+//
+//}
